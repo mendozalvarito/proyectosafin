771cabc9532d4e759800fb314cb092a45c0fc65c
ajerhy <ajerhy@gmail.com>
2019-01-21 09:41:58 -0800

    forms and views

---
+++ apps/clientes/view/views_direccion.py
@@ -0,0 +1,7 @@
+from django import forms
+from apps.clientes.models import Direccion
+
+"""
+Constantes
+"""
+
---
+++ apps/clientes/view/views_persona.py
@@ -0,0 +1,6 @@
+from django import forms
+from apps.clientes.models import Persona
+
+"""
+Constantes
+"""
