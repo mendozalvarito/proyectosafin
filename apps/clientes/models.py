97ba4a308b19013740cf013d4bd4bc73c97ded5f
ajerhy <ajerhy@gmail.com>
2019-01-23 19:27:54 -0400

    Modelos ADD

--- apps/clientes/models.py
+++ apps/clientes/models.py
@@ -29,6 +29,25 @@ class TimeStampModel(models.Model):
         abstract = True
 #TimeStampModel    title=True, |titlecase
 
+
+"""
+class TimeStamped(models.Model): 
+    creation_date = models.DateTimeField(editable=False) 
+    last_modified = models.DateTimeField(editable=False) 
+
+    def save(self, *args, **kwargs): 
+     if not self.creation_date: 
+      self.creation_date = timezone.now() 
+
+     self.last_modified = timezone.now() 
+     return super(TimeStamped, self).save(*args, **kwargs) 
+
+    class Meta: 
+     abstract = True 
+"""
+
+
+
 class Persona(TimeStampModel):
     nombrepersona = models.CharField(max_length=100, blank=False, null=True,
                                      verbose_name='Nombre',
